089a471442ea1024df66c2fd6222b9fbf4a57f4b
aswanth <aswanthas124@gmail.com>
2024-08-28 20:22:03 +0530

    tabview sample

--- LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/ContentView.swift
@@ -10,15 +10,45 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            Text("First view")
+            Group {
+                NavigationStack {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack {
+                            ForEach(0..<10) {_ in
+                                HStack {
+                                    Spacer()
+                                    Text("ttt")
+                                        .padding()
+                                    Spacer()
+                                }
+                            }
+                        }
+                    }
+                    .frame(height: .infinity)
+                    .navigationTitle("Checking")
+                    .toolbarBackground(.automatic, for: .navigationBar)
+                    .toolbarBackground(.gray, for: .navigationBar)
+                }
                 .tabItem { Label("First", systemImage: "1.circle") }
-            Text("Second view")
-                .tabItem { Label("Second", systemImage: "2.circle") }
+                
+                SecondView()
+                    .tabItem { Label("Second", systemImage: "2.circle") }
+            }
+            .toolbarBackground(.indigo, for: .tabBar)
+            .toolbarBackground(.visible, for: .tabBar)
+            .toolbarColorScheme(.light, for: .tabBar)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct SecondView: View {
+    var body: some View {
+        VStack {
+            Text("Second View")
+        }
+    }
 }
