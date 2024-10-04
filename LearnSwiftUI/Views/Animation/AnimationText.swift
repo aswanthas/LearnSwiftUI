9ab63b9dc27993237d5f396aa9f5cd37107ba30e
aswanth_shabari <aswanthas124@gmail.com>
2024-10-04 18:26:38 +0530

    text animation

---
+++ LearnSwiftUI/Views/Animation/AnimationText.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationText.swift
+//  LearnSwiftUI
+//
+//  Created by Aswanth K on 04/10/24.
+//
+
+import SwiftUI
+
+struct AnimationText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationText()
+}
